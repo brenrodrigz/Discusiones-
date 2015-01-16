0638751d9110c815a118885434c52524fa95ba6d
brenrodrigz <bar_772@hotmail.com>
2015-01-15 21:14:46 -0300

    first commit app django

---
+++ SistemaDiscusiones/settings/base.py
@@ -0,0 +1,36 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SECRET_KEY = 'b@77aq6jj!!y7*0zz6n5yt&&#w1%hqhyrb$l@w6md8drc+s(9m'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'SistemaDiscusiones.urls'
+
+WSGI_APPLICATION = 'SistemaDiscusiones.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
